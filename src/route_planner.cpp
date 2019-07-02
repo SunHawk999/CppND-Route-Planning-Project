cdbdab3741dc4c607a5ed37da5d65a5e44652e49
Ethan Williams <ethanw123@gmail.com>
2019-07-02 13:50:17 -0400

    Write variables and methods for RoutePlanner Constructor

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -3,4 +3,13 @@
 
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
 
+    //Scale floats to percentages
+    start_x *= 0.01;
+    start_y *= 0.01;
+    end_x *= 0.01;
+    end_y *= 0.01;
+
+    //Should be pointers? When attaching & to m_model, running make fails
+    start_node = m_Model.FindClosestNode(start_x, start_y);
+    end_node = m_Model.FindClosestNode(end_x, end_y);
 }
