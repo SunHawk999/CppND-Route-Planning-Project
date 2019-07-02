88adf1790154df857bbfb810f907ab18c5675881
Ethan Williams <ethanw123@gmail.com>
2019-07-02 13:34:35 -0400

    Write variables and methods for RoutePlanner Header

--- src/route_planner.h
+++ src/route_planner.h
@@ -10,8 +10,15 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
+    float GetDistance(){return distance;}
 
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+    //start and end node will point to the nodes in the model which are closest to the starting and ending points
+    RouteModel::Node start_node;
+    RouteModel::Node end_node;
+    //Will hold the total distance for the route that A* search finds from start to end
+    float distance;
+
 };
